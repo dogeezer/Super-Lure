2665215ad864a228a25d567b0b6c26df2b35c2c2
dogeezer <dogeealt@gmail.com>
2025-10-15 00:24:23 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,7 @@
+async function getShipping() {
+  const response = await fetch('/shipping');
+  const data = await response.json();
+  console.log(data); // or display it in HTML
+}
 
+getShipping();
