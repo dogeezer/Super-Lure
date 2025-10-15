31646e4a79a1c58b3c6a8fc024ce2228de61c2aa
dogeezer <dogeealt@gmail.com>
2025-10-14 22:23:36 -0400

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for homepage
 app.get('/', (req, res) => {
-  res.send('<h1>Super Lure is live!</h1><p>Welcome to the site.</p>');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Route for checkout page
+app.get('/checkout', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'checkout.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
